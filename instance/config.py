6416c8c6794579a2cde41027c7ac999360a55834
Manmeet Singh <manmeet3@gmail.com>
2019-12-15 19:59:35 -0800

    updated db config

--- instance/config.py
+++ instance/config.py
@@ -1,2 +1,2 @@
 SECRET_KEY = 'p9Bv<3Eid9%$i01'
-SQLALCHEMY_DATABASE_URI = 'mysql://root:Hehar408!@localhost/employees'
+SQLALCHEMY_DATABASE_URI = 'mysql://jeyasri:Jeyasri272!@ec2-54-183-185-86.us-west-1.compute.amazonaws.com:3306/employees'#'mysql://dt_admin:dt2016@localhost/dreamteam_db'
